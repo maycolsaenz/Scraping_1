0a902d8140c6e392a17fac2b950ae6b5a2a09b5b
maycolsaenz <maycol.saenz@ucr.ac.cr>
2022-10-25 17:12:34 -0600

    Envio de correo de alarma

--- main.py
+++ main.py
@@ -1,40 +1,72 @@
 # %%
 from bs4 import BeautifulSoup
 import requests
+import time
+from datetime import datetime
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 
 loginurl = ('https://131.101.120.134/dms2/Login.jsp')
-navigate = ('https://131.101.120.134/dms2/systemsetting/DeviceInformation_BACnet.jsp?address=11.13.04&deviceType=indoor')
 main = ('https://131.101.120.134/dms2/main/main.jsp')
+
+EDR1 = ('https://131.101.120.134/dms2/systemsetting/DeviceInformation_BACnet.jsp?address=11.13.01&deviceType=indoor')
+EDR2 = ('https://131.101.120.134/dms2/systemsetting/DeviceInformation_BACnet.jsp?address=11.13.05&deviceType=indoor')
+EDR3 = ('https://131.101.120.134/dms2/systemsetting/DeviceInformation_BACnet.jsp?address=11.13.04&deviceType=indoor')
+# IDF1 = ('https://131.101.120.134/dms2/systemsetting/DeviceInformation_BACnet.jsp?address=11.13.00&deviceType=indoor')
+# IDF2 = ('https://131.101.120.134/dms2/systemsetting/DeviceInformation_BACnet.jsp?address=11.13.03&deviceType=indoor')
+# IDF3 = ('https://131.101.120.134/dms2/systemsetting/DeviceInformation_BACnet.jsp?address=11.13.02&deviceType=indoor')
+#MDF = ('https://131.101.120.134/dms2/systemsetting/DeviceInformation_BACnet.jsp?address=11.02.04&deviceType=indoor')
+equipos = [EDR1, EDR2, EDR3]
+
+#%% envio de correo condicional
+sender = 'pythonMonitor@teradyne.com'
+receivers = ['maycol.saenz@teradyne.com']
+
+message = """From: pythonMonitor <pythonMonitor@teradyne.com>
+To: Usuario <maycol.saenz@teradyne.com>
+Subject: ALTA TEMPERATURA en {} 
+Temperatura ambiente en grados celsius es de: {} 
+
+Requiere atension inmediata
+Contacte a Facilities
+
+
+"""
+
+
+ 
+#%% Llamando variables
 session = requests.Session()
-
 jar = requests.cookies.RequestsCookieJar() 
-jar.set('JSESSIONID','kdtept3bdgvj7ar3e7gh6w1f')
-
+jar.set('JSESSIONID','byghu61ro7cl121fmjwtzvz89')
 session.cookies = jar
-r = session.get(navigate, verify = False).text
 
-#%% Encontrando direccion
-soup = BeautifulSoup(r,'lxml')
-Addr = soup.find('label', class_ = 'b fontSize12').get_text()
-
-
-#%% Encontrando nombre
-nombre = soup.find('td', id = 'OBJ_NAME').get_text()
-
-#%% Encontrando valor
-valor = soup.find('td', class_ = 'ACenter', id = 'A_VALUE_0').get_text()
-
-#%% Encontrando setpoint
-setpoint = soup.find('td', class_ = 'ACenter', id = 'A_VALUE_1').get_text()
-
-#%% Encontrando EDR #1 en main
-#EDR1 = soup.find('div', class_ = 'indoorBackground', id = 'indoorBackground')
-
-#%% Resultados
-print(Addr)
-print("Nombre = "+ nombre)
-print("Setpoint = " + setpoint)
-print("Valor actual = " + valor)
+for i in range(1):
+    for e in equipos:
+        r = session.get(e, verify = False).text
+        soup = BeautifulSoup(r,'html.parser')
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S ")
+        Addr = soup.find('label', class_ = 'b fontSize12').text.replace(' ','')
+        nombre = soup.find('td', id = 'OBJ_NAME').text.replace(' ','')
+        valor = soup.find('td', class_ = 'ACenter', id = 'A_VALUE_0').text.replace('[°C]','')
+        setpoint = soup.find('td', class_ = 'ACenter', id = 'A_VALUE_1').text.replace('[°C]','').strip()
+        salida = [str(i)+' ', current_time,Addr,nombre +' ', setpoint +' ', valor]
+        with open('historial.txt','a') as f:
+            for salidas in salida:
+                f.write(salidas)
+                print(salida)
+        if(int(valor)>=22):
+            try:
+                smtpObj = smtplib.SMTP('192.168.150.92')
+                smtpObj.sendmail(sender, receivers, message.format(nombre, valor))         
+                print ("Successfully sent email")
+            except SMTPException:
+                print ("Error: unable to send email")
+        
+    time.sleep(10)
 
 
 # %%
